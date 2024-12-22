d1d4eba88689833d37e5ec1944fe4dc4ed985924
xeeble <xeeble@hotmail.co.uk>
2024-12-22 12:06:42 -0500

    implemented get books,ISBN,Author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -15,19 +15,33 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.send(JSON.stringify({books},null,4));
+  res.send(JSON.stringify({books},null,4));
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get("/isbn/:isbn", async function (req, res) {
+    //Write your code here   
+    const ISBN = req.params.isbn;   
+    res.send(books[ISBN]);
+});
+  
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  new Promise((resolve, reject) => {
+    const author = req.params.author;
+    const booksByAuthor = Object.values(books).filter(book => book.author === author);
+    if (booksByAuthor.length === 0) {
+        reject("No books found by this author");
+    } else {
+        resolve(booksByAuthor);
+    }
+    }) .then(data => {
+        res.status(200).json(data);
+    }).catch(error => {
+        res.status(404).json({ message: "No books Found." });
+    });
 });
 
 // Get all books based on title
